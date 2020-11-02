39e9e85be0338cabec479d7bb589c656641e6430
Dan Ivovich <divovich@gmail.com>
2020-11-02 15:22:43 -0500

    WIP - provide more useful JSON to admin for analytics

--- app/controllers/analytics_controller.rb
+++ app/controllers/analytics_controller.rb
@@ -1,8 +1,17 @@
 class AnalyticsController < ApplicationController
   respond_to :json
-  def all
-    @visits = Ahoy::Visit.all.to_json
 
-    respond_with @visits
+  def all
+    homepage_views = Ahoy::Visit.where(landing_page: ENV['HOMEPAGE_URL']).count
+
+    params[:start_date] # reasonable defaults?
+    params[:end_date] # reasonable defaults?
+
+    new_homepage_views = Ahoy::Visit.where(landing_page: ENV['HOMEPAGE_URL']).where(inserted_at: start_end...end_date).count
+
+    respond_with {
+      homepage_views: homepage_views,
+      new_homepage_views: new_homepage_views
+    }
   end
 end
